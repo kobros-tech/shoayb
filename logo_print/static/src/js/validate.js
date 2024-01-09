a02a51a028dd77faf60327612c695cd49547c943
kobros-tech <alkobroslymohamed@gmail.com>
2024-01-09 22:12:41 +0300

    disable buttons of selected positions not to be repeated again

--- logo_print/static/src/js/validate.js
+++ logo_print/static/src/js/validate.js
@@ -13,6 +13,16 @@ odoo.define("logo.chooseLogoDialog", function(require) {
         },
 
         _dialog: function(evt) {
+            // before opening return all extra values to "no" and "no thanks"
+            document.querySelectorAll("select").forEach(function (element) {
+                if (ptal_printing_id === element.dataset.attribute_id) {
+                    
+                    // before opening the dialog
+                    setting_defaults()
+                }
+            });
+
+
             document.querySelector("#first_logo_table").style.display = "block";
             document.querySelector("#second_logo_table").style.display = "none";
             document.querySelector("#third_logo_table").style.display = "none";
