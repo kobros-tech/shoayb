57516577f1d91f2a6314b232f847909c4609d3ff
kobros-tech <alkobroslymohamed@gmail.com>
2024-01-26 01:08:20 +0300

    finalize product combination cache attributes

--- logo_print/static/src/js/validate.js
+++ logo_print/static/src/js/validate.js
@@ -6,33 +6,33 @@ odoo.define("logo.chooseLogoDialog", function(require) {
 
     var publicwidget = require("web.public.widget");
 
-    publicwidget.registry.chooseLogoDialog = publicwidget.Widget.extend({
-        selector: "#greeting_id",
-        events: {
-            'click': "_dialog",
-        },
+    // publicwidget.registry.chooseLogoDialog = publicwidget.Widget.extend({
+    //     selector: "#greeting_id",
+    //     events: {
+    //         'click': "_dialog",
+    //     },
 
-        _dialog: function(evt) {
-            // before opening return all extra values to "no" and "no thanks"
-            document.querySelectorAll("select").forEach(function (element) {
-                if (ptal_printing_id === element.dataset.attribute_id) {
+    //     _dialog: function(evt) {
+    //         // before opening return all extra values to "no" and "no thanks"
+    //         document.querySelectorAll("select").forEach(function (element) {
+    //             if (ptal_printing_id === element.dataset.attribute_id) {
                     
-                    // before opening the dialog
-                    setting_defaults()
-                }
-            });
+    //                 // before opening the dialog
+    //                 setting_defaults()
+    //             }
+    //         });
 
 
-            document.querySelector("#first_logo_table").style.display = "block";
-            document.querySelector("#second_logo_table").style.display = "none";
-            document.querySelector("#third_logo_table").style.display = "none";
-            document.querySelector("#fourth_logo_table").style.display = "none";
-            document.querySelector("#extra_logo_choice").style.display = "none";
-            document.querySelector("#extra2_logo_choice").style.display = "none";
-            document.querySelector("#extra3_logo_choice").style.display = "none";
-            document.querySelector("dialog[id='dialog_positions']").showModal();
-        },
-    });
+    //         document.querySelector("#first_logo_table").style.display = "block";
+    //         document.querySelector("#second_logo_table").style.display = "none";
+    //         document.querySelector("#third_logo_table").style.display = "none";
+    //         document.querySelector("#fourth_logo_table").style.display = "none";
+    //         document.querySelector("#extra_logo_choice").style.display = "none";
+    //         document.querySelector("#extra2_logo_choice").style.display = "none";
+    //         document.querySelector("#extra3_logo_choice").style.display = "none";
+    //         document.querySelector("dialog[id='dialog_positions']").showModal();
+    //     },
+    // });
 
     publicwidget.registry.closeLogoDialog = publicwidget.Widget.extend({
         selector: "#close_dialog",
