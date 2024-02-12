c6cecd26aa766ba68d4c7254cf9e597e51e4db8e
kobros-tech <alkobroslymohamed@gmail.com>
2024-02-12 20:50:37 +0300

    adding touches for module description and details

--- logo_print/__manifest__.py
+++ logo_print/__manifest__.py
@@ -1,9 +1,38 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    kobros-tech Pvt. Ltd.
+#
+#    Copyright (C) 2020-TODAY kobros-tech(<https://www.linkedin.com/in/mohamed-alkobrosly/>).
+#    Author: Mohamed Alkobrosli(<https://www.linkedin.com/in/mohamed-alkobrosly/>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (GPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU GENERAL PUBLIC LICENSE (GPL v3) for more details.
+#
+#    You should have received a copy of the GNU GENERAL PUBLIC LICENSE
+#    (GPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
 {
     'name':'Logo Print Extension',
     'category': 'Website/Website',
-    'version': '1.1',
+    'version': '16.0.0.0.1',
     'author': 'kobros-tech (Mohamed Moustafa Alkobrosli)',
-    'website':'http://www.kobros-tech.com',
+    'company': 'kobros-tech',
+    'contributors': [
+        'Mohamed Moustafa Alkobrosli <alkobroslymohamed@gmail.com>', 
+        'kobros-tech Pvt. Ltd. <info@kobros-tech.com>'
+    ],
+    'maintainer': 'Mohamed Moustafa Alkobrosli',
+    'website':'http://odoo.kobros-tech.com',
+    'license': 'GPL-3 or any later version',
     'summary': "Custom module for frontend & backend shop website, and backend system.",
     'sequence': 1,
 
@@ -12,6 +41,7 @@
     In addition to that, the administrator can upload the collection of logo positions on the website.
     """,
 
+    'images': ['static/description/banner.png'],
     'category':'Website',
     'depends': [
         'mail',
